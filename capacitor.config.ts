9e31ab05f45ac78353e2a9544fbcf8671d159b9f
quochuy278 <quochuy270800@gmail.com>
2023-02-22 21:02:26 +0200

    add capacitor android and ios

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gomunchi.app',
+  appName: 'go-munchi-merchant',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
