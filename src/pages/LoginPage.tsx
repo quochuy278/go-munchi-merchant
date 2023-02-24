7b3595f4353b2e0517e75f8554451b07ed41110b
Phong Ly <phong.ly@pinja.com>
2023-02-25 00:56:18 +0200

    Remove businessId when handle login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import LoginForm from '../components/LoginForm'
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { selectSession } from "../store/slices/session";
+
 
 export const LoginPage = () => {
-  console.log('This is login Page')
+
+  // The `state` arg is correctly typed as `RootState` already
+  const { userId } = useAppSelector(selectSession);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userId) {
+      navigate('/business', { replace: true })
+    }
+  }, [userId, navigate]);
+
   return (
     <LoginForm />
   )
