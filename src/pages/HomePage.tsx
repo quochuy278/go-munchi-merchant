fedf9758f714b65ab6b4ebebcdc6a720c6e4dcd7
quochuy278 <quochuy270800@gmail.com>
2023-02-27 14:26:14 +0200

    add dummy button to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,16 +2,16 @@ import CenterContainer from "../components/Container/CenterContainer";
 import { useAppSelector } from "../store/hooks";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { selectSession, set as setSessionState } from "../store/slices/session";
-
+import {Button} from '@mui/material'
 
 export const HomePage = () => {
 
-  const { businessId } = useAppSelector(selectSession);
+  // const { businessId } = useAppSelector(selectSession);
 
   console.log("This is home page");
   return (
     <CenterContainer>
-      <div>This home page</div>
+      <Button variant="contained"> Ringing button</Button>
     </CenterContainer>
   );
 };
