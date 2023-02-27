c2742845f37f6dd7f496a0ffc2e8218145a5ef3d
quochuy278 <quochuy270800@gmail.com>
2023-02-27 22:23:39 +0200

    add useWebSocker in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,39 +9,65 @@ import Modal from "../components/Dialog/Dialog";
 import FactoryDialog from "../components/Dialog/Dialog";
 import useSound from "use-sound";
 import music from "../assets/sounds/song.mp3";
+import {  io } from "socket.io-client";
+import useWebSocket, { ReadyState, useSocketIO } from "react-use-websocket";
 
 export const HomePage = () => {
+  const [socketUrl, setSocketUrl] = useState("http://localhost:5000/");
+  const { sendMessage, lastMessage, readyState } = useSocketIO("http://localhost:5000/", {share:true}, true);
+  console.log(lastMessage, readyState)
+  // const socket = io("http://localhost:5000/");
+  // socket.on('connect', (socket) => {
+  //   socket.emit('join', room)
+  // })
+
+  // socket.on("newOrder", (socket) => {
+  //   if (socket) {
+  //     console.log(socket, "line 17");
+  //     onHandleModal();
+  //   }
+  // });
+  //join()
   const [modal, setModal] = useState<boolean>(false);
   // const { businessId } = useAppSelector(selectSession);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [playBoop, { stop, pause }] = useSound(music, {
-    volume: 0.25,
+    volume: 1,
+    interrupt: false,
     onplay: () => setIsPlaying(true),
-    onend: () => setIsPlaying(false),
+    loop: true,
   });
   const onCloseDialog = () => {
     setModal(false);
     pause();
   };
-  console.log("This is home page");
+  const onStop = () => {
+    setModal(false);
+    setIsPlaying(false);
+    stop();
+    // audio.pause();
+    // audio.currentTime = 0;
+  };
   const onHandleModal = () => {
+    // audio.loop = true;
+    // audio.play();
     setModal(true);
     setIsPlaying(true);
     playBoop();
   };
-  const onHandleClick = async (event: any) => {
-    await LocalNotifications.schedule({
-      notifications: [
-        {
-          id: 1,
-          title: "Tesing from Huy",
-          body: "tesing this shitty functionality",
-          sound: "beep.wav",
-          autoCancel: true,
-        },
-      ],
-    });
-  };
+  // const onHandleClick = async (event: any) => {
+  //   await LocalNotifications.schedule({
+  //     notifications: [
+  //       {
+  //         id: 1,
+  //         title: "Tesing from Huy",
+  //         body: "tesing this shitty functionality",
+  //         sound: "beep.wav",
+  //         autoCancel: true,
+  //       },
+  //     ],
+  //   });
+  // };
 
   //showmodel
   return (
@@ -49,7 +75,9 @@ export const HomePage = () => {
       <Button variant="contained" onClick={onHandleModal}>
         Ringing button
       </Button>
-      <Button variant="contained">Stop button</Button>
+      <Button variant="contained" onClick={onStop}>
+        Stop button
+      </Button>
       {modal && (
         <FactoryDialog
           open={modal}
