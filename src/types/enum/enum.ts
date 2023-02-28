1496cdd117b91bbdaca4b4bbb4d25d9da5f61634
quochuy278 <quochuy270800@gmail.com>
2023-02-28 21:54:21 +0200

    add enum for order and delivery

---
+++ src/types/enum/enum.ts
@@ -0,0 +1,19 @@
+export enum OrderEnum {
+    PENDING = 0,
+    COMPLETED = 1,
+    REJECTED = 2,
+    PREPARATION_COMPLETED = 4,
+    REJECTED_BY_BUSINESS = 5,
+    ACCEPTED_BY_BUSINESS = 7,
+    ACCEPTED_BY_DRIVER =8,
+    PICK_UP_COMPLETED_BY_DRIVER = 9,
+    PICK_UP_FAILED_BY_DRIVER = 10,
+}
+
+export enum DeliveryEnum {
+    DELIVERY = 1,
+    PICK_UP = 2,
+    EATIN = 3,
+    CURBSIDE = 4,
+    DRIVER_THRU = 5,
+}
