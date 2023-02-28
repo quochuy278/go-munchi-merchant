2dcec8a1a28b6e1eb128f43bea22122ffd778917
quochuy278 <quochuy270800@gmail.com>
2023-02-28 22:12:24 +0200

    add right board list in order detail board right

---
+++ src/components/OrderDetail/OrderDetailBoard/OrderDetailBoardRight/OrderDetailBoardRightList/OrderDetailBoardRightList.tsx
@@ -0,0 +1,60 @@
+import { Box, Typography } from "@mui/material";
+import { IconFactory } from "../../../../Factory/icons/IconFactory";
+import styles from './OrderDetailBoardRightList.module.css'
+
+
+export interface OrderDetailRightListProps {
+  name: string;
+  deliveryType: number;
+}
+const OrderDetailBoardRightList = ({
+  name,
+  deliveryType,
+}: OrderDetailRightListProps) => {
+  return (
+    <Box className={styles.detail__info}>
+      <Box className={styles.detail__info__bar}>
+        <Box sx={{ marginX: "10px" }}>
+          <Typography fontSize="10px" lineHeight="13px">
+            Customer
+          </Typography>
+          <Typography fontSize="18px" lineHeight="24px">
+            {name}
+          </Typography>
+        </Box>
+        {/* <IconButton
+          sx={{
+            marginX: "10px",
+            backgroundColor: "#F2F9FE",
+            borderRadius: "8px",
+            width: "40px",
+          }}
+        >
+          <PhoneOutlinedIcon
+            sx={{ width: "14px", height: "14px" }}
+            color="primary"
+          />
+        </IconButton>
+        <IconButton
+          sx={{
+            marginX: "10px",
+            backgroundColor: "#F2F9FE",
+            borderRadius: "8px",
+            width: "40px",
+          }}
+        >
+          <ChatBubbleOutlineOutlinedIcon
+            sx={{ width: "14px", height: "14px" }}
+            color="primary"
+          />
+        </IconButton> */}
+      </Box>
+
+      <Box sx={{ padding: "20px" }}>
+        <IconFactory deliveryType={deliveryType} />
+      </Box>
+    </Box>
+  );
+};
+
+export default OrderDetailBoardRightList;
