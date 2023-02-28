e18ea96a0ec9fdefecc45a463b1828103caeaed5
quochuy278 <quochuy270800@gmail.com>
2023-02-28 22:09:27 +0200

    add order detail board left

---
+++ src/components/OrderDetail/OrderDetailBoard/OrderDetailBoardLeft/OrderDetailBoardLeft.tsx
@@ -0,0 +1,67 @@
+import { Box, Divider, IconButton, Typography } from "@mui/material";
+import React from "react";
+import styles from "./OrderDetailBoardLeft.module.css";
+import PrintIcon from "@mui/icons-material/Print";
+import CloseIcon from "@mui/icons-material/Close";
+import { LeftBoardFooter } from "./LeftBoardFooter/LeftBoardFooter";
+import { LeftBoardList } from "./LeftBoardList/LeftBoardList";
+import FullscreenLoading from "../../../Loading/FullscreenLoading";
+
+const OrderDetailBoardLeft = ({ data }: any) => {
+  if (!data) {
+    return <FullscreenLoading />;
+  }
+  return (
+    <Box gridColumn="span 6" className={styles.detail__content}>
+      <Box className={styles.detail__card}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          className={styles.detail__title}
+        >
+          <Typography fontSize="18px" lineHeight="24px" fontWeight={600}>
+            {data?.products.length} Items
+          </Typography>
+          {/* <IconButton sx={{ backgroundColor: "#F2F9FE", borderRadius: "8px" }}>
+            <PrintIcon
+              sx={{ marginRight: "10px", width: "14px", height: "14px" }}
+              color="primary"
+            />
+            <Typography fontSize="12px" lineHeight="16px">
+              Print
+            </Typography>
+          </IconButton> */}
+        </Box>
+        <Divider sx={{ width: "100%", marginTop: "10px" }} />
+        <LeftBoardList products={data.products} />
+        <Box sx={{ width: "90%" }}>
+          <Box
+            sx={{
+              backgroundColor: "#F3F5F7",
+              width: "fit-content",
+              height: "fit-content",
+              padding: "8px",
+              borderRadius: "8px",
+            }}
+          >
+            <Typography
+              fontSize="12px"
+              lineHeight="16px"
+              fontFamily="DM-sans"
+              fontWeight={600}
+            >
+              {data?.products.comments}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box className={styles.payment_container}>
+        <Divider sx={{ width: "100%", marginBottom: "20px" }} />
+        <LeftBoardFooter />
+      </Box>
+    </Box>
+  );
+};
+
+export default OrderDetailBoardLeft;
