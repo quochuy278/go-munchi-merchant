c3cbf360aa0e0d0aa2ff17119522118ae4908b0b
quochuy278 <quochuy270800@gmail.com>
2023-02-28 21:52:53 +0200

    add coundown clock component

---
+++ src/components/CountDownClock/CountDownClock.tsx
@@ -0,0 +1,42 @@
+import { Typography, Box } from "@mui/material";
+import { useState } from "react";
+import { useCountdown } from "../../hooks/useCountDown";
+import { TimeFormatFactory } from "../Factory/time/TimeFormatFactory";
+
+interface ClockProps {
+  targetDate: number;
+}
+
+interface CoundownProps {
+  targetDate: number;
+}
+
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
+};
+
+const CountdownTimer = ({ targetDate }: CoundownProps) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <TimeFormatFactory
+        minutes={minutes}
+        seconds={seconds}
+        isDanger={minutes < 3}
+      />
+    );
+  }
+};
+const CountDownClock = ({ targetDate }: ClockProps) => {
+  return <CountdownTimer targetDate={targetDate} />;
+};
+
+export default CountDownClock;
