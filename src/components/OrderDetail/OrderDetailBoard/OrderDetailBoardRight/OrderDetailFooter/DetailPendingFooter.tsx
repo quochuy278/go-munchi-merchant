3f1cddaed256ba937abea33206033c66b224935b
quochuy278 <quochuy270800@gmail.com>
2023-03-02 17:41:58 +0200

    add detail order footer

---
+++ src/components/OrderDetail/OrderDetailBoard/OrderDetailBoardRight/OrderDetailFooter/DetailPendingFooter.tsx
@@ -0,0 +1,251 @@
+import React, { useState } from "react";
+import { Box, Typography, TextField, Button } from "@mui/material";
+import styles from "./DetailFooter.module.css";
+export interface DetailFooterProps {
+  timeStamp: string;
+  orderStatus: number;
+  deliveryType: number;
+  orderId: number;
+}
+const timeAvailable = [1, 5, 10, 15, 20, 30];
+const DetailPendingFooter = ({
+  orderStatus,
+  timeStamp,
+  orderId,
+  deliveryType,
+}: DetailFooterProps) => {
+  const [inputVisible, setInputVisible] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [newPrepTime, setNewPrepTime] = useState(10);
+  const InputVisibleHandler = () => {
+    setInputVisible(true);
+  };
+  const setTimeHandler = (event: any, time: number) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (time) {
+      setNewPrepTime(time);
+      setInputVisible(false);
+    } else {
+      setNewPrepTime(event.target.value);
+    }
+  };
+
+  const acceptHandler = () => {
+    setOpen(true);
+  };
+
+  const acceptDialogCloseHandler = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Box className={styles.detail__time__info}>
+        <Typography fontSize="10px" lineHeight="13px" fontWeight={500}>
+          Create at {timeStamp}
+        </Typography>
+      </Box>
+
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(4, 1fr)"
+        sx={{
+          padding: "20px",
+          height: "calc(100% - 80px)",
+        }}
+        gap={1}
+        rowGap={1}
+      >
+        <Box
+          gridColumn="span 4"
+          sx={{ padding: 0 }}
+          display="flex"
+          alignItems="center"
+        >
+          <Typography
+            fontSize="20px"
+            lineHeight="26px"
+            fontWeight={800}
+            fontFamily="DM-sans-bold"
+          >
+            Ready in
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            label="Time"
+            variant="outlined"
+            {...(inputVisible
+              ? { sx: { marginLeft: "20px" } }
+              : { sx: { marginLeft: "20px", display: "none" } })}
+            type="number"
+            inputProps={{ maxLength: 3 }}
+            onChange={(e: any) => {
+              setNewPrepTime(e.target.value);
+            }}
+          />
+        </Box>
+        {timeAvailable.map((time, index) => {
+          return (
+            <Box gridColumn="span 1" key={`${index}`}>
+              <Button
+                variant="contained"
+                className={styles.time__btn}
+                {...(time === newPrepTime
+                  ? {
+                      sx: {
+                        backgroundColor: "#F1F6ED",
+                        color: "black",
+                        padding: 0,
+                        borderRadius: "8px",
+                        boxShadow: "none",
+
+                        "&:focus": {
+                          backgroundColor: "#F1F6ED",
+                          color: "#74A047",
+                        },
+                        "&:active": {
+                          backgroundColor: "#5D8139",
+                          color: "white",
+                        },
+                        "&.MuiButton-selected": {
+                          backgroundColor: "#F1F6ED",
+                          color: "#74A047",
+                        },
+                        "&:hover": {
+                          backgroundColor: "#759F47",
+                          color: "#F1F6ED",
+                        },
+                      },
+                    }
+                  : {
+                      sx: {
+                        backgroundColor: "#F3F5F7",
+                        color: "black",
+                        padding: 0,
+                        borderRadius: "8px",
+                        boxShadow: "none",
+                        "&:focus": {
+                          backgroundColor: "#F1F6ED",
+                          color: "#74A047",
+                        },
+                        "&:active": {
+                          backgroundColor: "#5D8139",
+                          color: "white",
+                        },
+                        "&.MuiButton-selected": {
+                          backgroundColor: "#F1F6ED",
+                          color: "#74A047",
+                        },
+                        "&:hover": {
+                          backgroundColor: "#759F47",
+                          color: "#F1F6ED",
+                        },
+                      },
+                    })}
+                onClick={(event) => setTimeHandler(event, time)}
+              >
+                <Box>
+                  <Typography
+                    fontSize="20px"
+                    lineHeight="26px"
+                    fontFamily="DM-sans-bold"
+                  >
+                    {time}
+                  </Typography>
+                  <Typography
+                    fontSize="8px"
+                    lineHeight="10px"
+                    textTransform="none"
+                  >
+                    mins
+                  </Typography>
+                </Box>
+              </Button>
+            </Box>
+          );
+        })}
+
+        <Box gridColumn="span 2">
+          <Button
+            variant="contained"
+            className={styles.time__btn}
+            sx={{
+              backgroundColor: "#F3F5F7",
+              color: "black",
+              width: "100%",
+              borderRadius: "8px",
+              boxShadow: "none",
+              "&:hover": {
+                color: "white",
+              },
+            }}
+            onClick={InputVisibleHandler}
+          >
+            <Typography>Custom</Typography>
+          </Button>
+        </Box>
+        <Box gridColumn="span 2">
+          <Button
+            variant="contained"
+            className={styles.time__btn}
+            sx={{
+              backgroundColor: "#FDF4F3",
+              color: "black",
+              width: "100%",
+              borderRadius: "8px",
+              boxShadow: "none",
+              "&:active": {
+                backgroundColor: "#FF2828",
+                svg: {
+                  color: "white",
+                },
+              },
+              "&:focus": {
+                border: "none",
+              },
+              "&:hover": {
+                backgroundColor: "#FF2828",
+                color: "white",
+              },
+            }}
+            disableFocusRipple={true}
+            disableTouchRipple={true}
+          >
+            <Typography>Decline</Typography>
+          </Button>
+        </Box>
+        <Box gridColumn="span 2">
+          <Button
+            {...(newPrepTime ? { variant: "contained" } : { disabled: true })}
+            variant="contained"
+            className={styles.time__btn}
+            sx={{
+              backgroundColor: "#F3F5F7",
+              color: "black",
+              width: "100%",
+              borderRadius: "8px",
+              boxShadow: "none",
+              "&:hover": {
+                color: "white",
+              },
+            }}
+            onClick={acceptHandler}
+          >
+            <Typography>Accept</Typography>
+          </Button>
+          {/* <DialogAlert
+              open={open}
+              newPrepTime={newPrepTime}
+              onClose={acceptDialogCloseHandler}
+              orderId={orderId}
+              deliveryType={deliveryType}
+              orderStatus={orderStatus}
+              prepTime={newPrepTime}
+            /> */}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default DetailPendingFooter;
