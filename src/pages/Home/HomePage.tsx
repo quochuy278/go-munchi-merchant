f18a5715b4edcaf05fc6903cd102bf74a665b407
quochuy278 <quochuy270800@gmail.com>
2023-03-03 20:31:05 +0200

    remove dialog in Home Page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -5,26 +5,7 @@ import CenterContainer from "../../components/Container/CenterContainer";
 import Order from "../../components/Order/Order";
 
 export const HomePage = () => {
-  const [modal, setModal] = useState<boolean>(false);
-  const [playBoop, { stop, pause }] = useSound(music, {
-    volume: 1,
-    interrupt: false,
-    loop: true,
-  });
-  const onCloseDialog = () => {
-    setModal(false);
-    pause();
-  };
-  const onStop = () => {
-    setModal(false);
 
-    stop();
-  };
-  const onHandleModal = () => {
-    setModal(true);
-
-    playBoop();
-  };
 
   return (
     <CenterContainer>
