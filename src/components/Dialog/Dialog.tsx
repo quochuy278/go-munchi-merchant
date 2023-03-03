97ed0bb01ff79bb7c01fb4fc6c173e948b2c2ade
quochuy278 <quochuy270800@gmail.com>
2023-03-03 20:27:40 +0200

    add dialog

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -8,32 +8,71 @@ import {
   LinearProgress,
 } from "@mui/material";
 import React from "react";
+import BusinessDialog from "./BusinessDialog/BusinessDialog";
 import styles from "./dialog.module.css";
+import OrderDialog from "./OrderDialog/OrderDialog";
 
-interface DialogData {
-  data: any;
+interface OrderDialogData {
+  deliveryType: number;
+  status: number;
+  orderId: number;
+  newPrepTime?: number | string;
+}
+interface BusinessDialogData {
+  name: string;
+  publicId: string;
 }
 
 interface DialogProps {
-  children?: JSX.Element | JSX.Element[];
   open: boolean;
-  modalData: DialogData;
+  children?: JSX.Element | JSX.Element[];
+  isOrder?: boolean;
+  isBusiness?: boolean;
+  modalData?: OrderDialogData | BusinessDialogData;
   onClose: () => void;
 }
 
-const FactoryDialog = ({ open, onClose, modalData }: DialogProps) => {
-  return (
-    <Dialog open={true} fullWidth={true} maxWidth={"sm"}>
-      <DialogTitle>Dialog testing</DialogTitle>
-      <DialogContent>{modalData.data}</DialogContent>
-      <DialogActions>
-    
-        <Button onClick={onClose}>Close</Button>
-       
-      </DialogActions>
-      <LinearProgress color="secondary" />
-    </Dialog>
-  );
+export interface OrderDialogProps extends DialogProps {
+  modalData?: any;
+  onClose: () => void;
+}
+
+export interface BusinessDialogProps extends DialogProps {
+  modalData: any;
+  onClose: () => void;
+}
+
+const FactoryDialog = ({
+  open,
+  onClose,
+  modalData,
+  isOrder,
+  isBusiness,
+}: DialogProps) => {
+  if (!modalData) {
+    return <p>No data</p>;
+  }
+  switch (true) {
+    case isOrder:
+      return (
+        <OrderDialog open={open} onClose={onClose} modalData={modalData} />
+      );
+    case isBusiness:
+      return (
+        <BusinessDialog open={open} onClose={onClose} modalData={modalData} />
+      );
+    default:
+      return (
+        <Dialog open={open} fullWidth={true} maxWidth={"sm"}>
+          <DialogTitle>Something wrong happened</DialogTitle>
+          <DialogContent>Please reset the page or login again</DialogContent>
+          <DialogActions>
+            <Button onClick={onClose}>Close</Button>
+          </DialogActions>
+          <LinearProgress color="secondary" />
+        </Dialog>
+      );
+  }
 };
 
 export default FactoryDialog;
