bdfe14e27e675b7e882392ba5437607777c8799b
quochuy278 <quochuy270800@gmail.com>
2023-03-08 20:04:44 +0200

    add error handler in business list

--- src/components/Business/BusinessList.tsx
+++ src/components/Business/BusinessList.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/system";
 import React from "react";
+import CenterContainer from "../Container/CenterContainer";
+import FullscreenLoading from "../Loading/FullscreenLoading";
 import BusinessItem from "./BusinessItem";
-import styles from './businesslist.module.css'
+import styles from "./businesslist.module.css";
 const BusinessList = ({ data }: any) => {
-    if (!data){
-        return <p>Loading tiny </p>
-    }
+  if (!data) {
+    return <CenterContainer>Something happend</CenterContainer>;
+  }
   return (
     <Box className={styles.business__list}>
       {data.map((business: any) => {
