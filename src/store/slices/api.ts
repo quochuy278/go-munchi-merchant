f358327bd4de0c01c9bea1b9c764d3c9801e9976
quochuy278 <quochuy270800@gmail.com>
2023-03-08 20:02:10 +0200

    add fetch base query with auth to handle token expire

--- src/store/slices/api.ts
+++ src/store/slices/api.ts
@@ -1,35 +1,89 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "..";
 import { FilterQuery } from "../../components/Order/OrderList/OrderList";
 import { UpdateOrderData } from "../../types";
+import axios from "axios";
+import { clearSessionState, setSessionState } from "./session";
+import { Mutex } from "async-mutex";
+import {
+  addSessionState,
+  clearSessionState as clearSession,
+} from "../../utils/preference";
 
 export interface SignInData {
   email: string;
   password: string;
 }
-
+const mutex = new Mutex();
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  //
+  await mutex.waitForUnlock();
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    // try to get a new token
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      const refreshToken = (api.getState() as RootState).session.refreshToken;
+      try {
+        const refreshResult = await axios({
+          url: `${process.env.REACT_APP_API__URL_DEV}auth/refreshToken`,
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        });
+        if (refreshResult.data) {
+          api.dispatch(
+            setSessionState({
+              refreshToken: refreshResult.data.refreshToken,
+              verifyToken: refreshResult.data.verifyToken,
+            })
+          );
+          await addSessionState({
+            refreshToken: refreshResult.data.refreshToken,
+            verifyToken: refreshResult.data.verifyToken,
+          });
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions);
+        } else {
+          api.dispatch(clearSessionState()); //logout and clear all data
+          await clearSession();
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release();
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+  return result;
+  // Define a service using a base URL and expected endpoints
+};
 const baseQuery = fetchBaseQuery({
   baseUrl: `${process.env.REACT_APP_API__URL_DEV}`,
   async prepareHeaders(headers, { getState }) {
-    // If we have a token set in state, let's assume that we should be passing it.
-    // const authenticateData: GetResult = await Preferences.get({ key: 'loginState' })
-    // const data: LoginState = JSON.parse(authenticateData.value!)
-    // var verifyToken: string
     const verifyToken = (getState() as RootState).session.verifyToken;
-    // if (!data)
-    //     verifyToken = loginState.verifyToken as string
+
     headers.set("authorization", `Bearer ${verifyToken}`);
-    // } else {
-    //     verifyToken = data.verifyToken as string
-    //     headers.set('authorization', `Bearer ${verifyToken}`)
-    // }
-    // return headers
   },
 });
 
 export const munchiApi = createApi({
   reducerPath: "munchiApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   tagTypes: ["Business", "User", "Order"],
   endpoints: (builder) => ({
     signIn: builder.mutation({
@@ -66,18 +120,22 @@ export const munchiApi = createApi({
       providesTags: ["Order"],
     }),
     updateOrder: builder.mutation({
-      query: (data: UpdateOrderData) => (
-        console.log(data),
-        {
-          url: `orders/${data.orderId}`,
-          method: "PUT",
-          body: {
-            preparedIn: data.newPrepTime,
-            orderStatus: data.orderStatus,
-          },
-        }
-      ),
+      query: (data: UpdateOrderData) => ({
+        url: `orders/${data.orderId}`,
+        method: "PUT",
+        body: {
+          preparedIn: data.newPrepTime,
+          orderStatus: data.orderStatus,
+        },
+      }),
       invalidatesTags: ["Order"],
+    }),
+    getRefreshToken: builder.query({
+      query: () => ({
+        url: `auth/refreshToken`,
+        method: "GET",
+      }),
+      providesTags: ["Order"],
     }),
   }),
 });
@@ -88,4 +146,5 @@ export const {
   useGetFilteredOrderQuery,
   useGetOrderByPublicIdQuery,
   useUpdateOrderMutation,
+  useGetRefreshTokenQuery,
 } = munchiApi;
