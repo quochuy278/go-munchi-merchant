4b937ee4a3b8e9f6bbdc82841aaef78344f34048
quochuy278 <quochuy270800@gmail.com>
2023-03-11 20:50:57 +0200

    remove unused import

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import useSound from "use-sound";
-import music from "../../assets/sounds/song.mp3";
 import CenterContainer from "../../components/Container/CenterContainer";
 import Order from "../../components/Order/Order";
 
