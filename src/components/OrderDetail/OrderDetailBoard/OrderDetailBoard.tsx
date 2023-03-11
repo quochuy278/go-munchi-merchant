70c054e43c122a32df42615450a30e7969636e9c
quochuy278 <quochuy270800@gmail.com>
2023-03-11 20:53:17 +0200

    add order detail board and remove the old one

--- src/components/OrderDetail/OrderDetailBoard/OrderDetailBoard.tsx
+++ src/components/OrderDetail/OrderDetailBoard/OrderDetailBoard.tsx
@@ -1,15 +1,44 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import React from "react";
-import styles from './OrderDetailBoard.module.css'
-export const OrderDetailBoard = ({ children }: any) => {
+import FullscreenLoading from "../../Loading/FullscreenLoading";
+import OrderDetailProducList from "../OrderDetailProductList/OrderDetailProducList";
+import OrderDetailSummary from "../OrderDetailSummary/OrderDetailSummary";
+import styles from "./OrderDetailBoard.module.css";
+export const OrderDetailBoard = ({ data }: any) => {
+  if (!data) {
+    return <FullscreenLoading />;
+  }
   return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"
-      gap={2}
-      className={styles.detail__container}
-    >
-      {children}
+    <Box gridColumn="span 6" className={styles.detail__content}>
+      <Box className={styles.detail__card}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          className={styles.detail__title}
+        >
+          <Typography fontSize="18px" lineHeight="24px" fontWeight={600}>
+            {data?.products.length} Items
+          </Typography>
+          {/* <IconButton sx={{ backgroundColor: "#F2F9FE", borderRadius: "8px" }}>
+            <PrintIcon
+              sx={{ marginRight: "10px", width: "14px", height: "14px" }}
+              color="primary"
+            />
+            <Typography fontSize="12px" lineHeight="16px">
+              Print
+            </Typography>
+          </IconButton> */}
+        </Box>
+        <Divider sx={{ width: "100%", marginTop: "10px" }} />
+
+        <OrderDetailProducList products={data.products} />
+      </Box>
+      <Box className={styles.payment_container}>
+        <Divider sx={{ width: "100%", marginBottom: "20px" }} />
+
+        <OrderDetailSummary summary={data.summary} />
+      </Box>
     </Box>
   );
 };
