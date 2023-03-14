39b26993bbc4ad948517228ac18507cd2f04e765
quochuy278 <quochuy270800@gmail.com>
2023-03-14 16:26:30 +0200

    fix socket and add refetch to order

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -6,6 +6,7 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Typography,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSocket } from "socket.io-react-hook";
@@ -16,6 +17,8 @@ import { selectSession } from "../../store/slices/session";
 import { OrderModel } from "../../types";
 import OrderContainer from "../Container/OrderContainer";
 import OrderSection from "./OrderSection/OrderSection";
+import style from "./Order.module.css";
+import ProductFactory from "../Factory/product/ProductFactory";
 export const pendingStatus: number[] = [0];
 export const acceptedStatus: number[] = [7, 8];
 export const completedStatus: number[] = [1, 4, 9, 11, 15];
@@ -26,6 +29,7 @@ const Order = () => {
     `${process.env.REACT_APP_API__URL_DEV}`
   );
   const [open, setOpen] = useState<boolean>(false);
+  const [refetch, setRefetch] = useState<boolean>(false);
   const [newOrder, setNewOrder] = useState<OrderModel>();
   const [play, { stop }] = useSound(music, {
     volume: 1,
@@ -35,16 +39,23 @@ const Order = () => {
 
   useEffect(() => {
     socket.emit("join", businessId);
-    socket.on("receive-order", (socket) => {
+    socket.on("new-order-notification", (socket) => {
       console.log(socket);
+
       play();
+      setRefetch(true);
       setOpen(true);
       setNewOrder(socket);
+    });
+    socket.on("order-status-change", (socket) => {
+      console.log(socket);
     });
   }, [socket, newOrder, open]);
 
   const onCloseDialog = () => {
     stop();
+    setRefetch(false);
+
     setOpen(false);
   };
 
@@ -61,29 +72,51 @@ const Order = () => {
           isCompleted={false}
           span={6}
           title={"Pending"}
+          refetch={refetch}
         />
         <OrderSection
           status={acceptedStatus}
           isCompleted={false}
           span={6}
           title={"On Progress"}
+          refetch={refetch}
         />
         <OrderSection
           status={completedStatus}
           isCompleted={true}
           span={2}
           title={"Completed"}
+          refetch={refetch}
         />
       </Box>
       {newOrder ? (
         <Dialog open={open} fullWidth={true} maxWidth={"sm"}>
-          <DialogTitle>New Order is here</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Customer: {newOrder.customer.name}
-              Your new order detail {newOrder.id}
-              Total: {newOrder.summary.total}
-            </DialogContentText>
+          <DialogTitle>New Order is here </DialogTitle>
+          <DialogContent className={style.dialog__content__container}>
+            <Box className={style.dialog__content}>
+              <Box>
+                <Typography variant="h6"> Order: # {newOrder.id}</Typography>
+                <Typography variant="h6">
+                  {" "}
+                  Customer: {newOrder.customer.name}{" "}
+                  {newOrder.customer.lastName}
+                </Typography>
+              </Box>
+              <Box className={style.dialog__content__product}>
+              <Typography variant="h6">
+                 Order Detail
+                </Typography>
+                <ProductFactory
+                  orderId={newOrder.id}
+                  product={newOrder.products}
+                />
+              </Box>
+            </Box>
+            <Box className={style.dialog__content__summary}>
+              <Typography variant="h6">
+                Total: {newOrder.summary.total} €
+              </Typography>
+            </Box>
 
             <DialogContentText></DialogContentText>
           </DialogContent>
